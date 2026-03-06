aa9e0052d0ec536ccb27ee4a78f0d7df1d7aaf75
mlil <mlil@itu.dk>
2026-03-06 11:02:51 +0100

    style(footer): added icon images and social media icons

--- frontend/src/app/core/layout/footer/footer.ts
+++ frontend/src/app/core/layout/footer/footer.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { siInstagram, siX, siYoutube, siFacebook } from 'simple-icons/icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  templateUrl: './footer.html'
+  templateUrl: './footer.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  protected readonly paymentIcons = [
+    { name: 'Visa', src: '/images/visa.png' },
+    { name: 'Mastercard', src: '/images/mastercard.png' },
+    { name: 'Google Pay', src: '/images/googlepay.png' },
+    { name: 'PayPal', src: '/images/paypal.jpg' },
+    { name: 'Apple Pay', src: '/images/applepay.png' },
+    { name: 'Stripe', src: '/images/stripe.png' },
+  ];
+
+  protected readonly socials = [
+    { name: 'Facebook', href: 'https://facebook.com', icon: siFacebook },
+    { name: 'X', href: 'https://x.com', icon: siX },
+    { name: 'YouTube', href: 'https://youtube.com', icon: siYoutube },
+    { name: 'Instagram', href: 'https://instagram.com', icon: siInstagram },
+  ];
+}
